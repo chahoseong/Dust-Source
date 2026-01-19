b2ded3cd1b0fcb5b961ac33510e4791b8480cde8
Cha Hoseong <hoseong@chahoseong.com>
2026-01-19 20:44:41 +0900

    Add log categories and utility function

---
+++ Dust/DustLogChannels.cpp
@@ -0,0 +1,32 @@
+﻿#include "DustLogChannels.h"
+
+DEFINE_LOG_CATEGORY(LogDust);
+DEFINE_LOG_CATEGORY(LogDustAbilitySystem);
+
+FString GetClientServerContextString(UObject* ContextObject)
+{
+	ENetRole Role  = ROLE_None;
+	
+	if (const AActor* Actor = Cast<AActor>(ContextObject))
+	{
+		Role = Actor->GetLocalRole();
+	}
+	else if (const UActorComponent* Component = Cast<UActorComponent>(ContextObject))
+	{
+		Role = Component->GetOwnerRole();
+	}
+	
+	if (Role != ROLE_None)
+	{
+		return Role == ROLE_Authority ? TEXT("Server") : TEXT("Client");
+	}
+#if WITH_EDITOR
+	if (GIsEditor)
+	{
+		extern ENGINE_API FString GPlayInEditorContextString;
+		return GPlayInEditorContextString;
+	}
+#endif
+	
+	return TEXT("[]");
+}
---
+++ Dust/DustLogChannels.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "Logging/LogMacros.h"
+
+DUST_API DECLARE_LOG_CATEGORY_EXTERN(LogDust, Log, All);
+DUST_API DECLARE_LOG_CATEGORY_EXTERN(LogDustAbilitySystem, Log, All);
+
+DUST_API FString GetClientServerContextString(UObject* ContextObject = nullptr);
